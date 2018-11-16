453ca4a07a4f6b27d2f74abc37434e043b6ea908
Roman <hafaml@Mail.ru>
2018-11-16 08:46:23 +0500

    update to python 3.7 make some collatz program

--- first_practice/swordfish.py
+++ first_practice/swordfish.py
@@ -1,5 +1,20 @@
-# practice some continue operation
-i: int = 10
-while i > 0:
-    print ( i )
-    i -= 1
+# practice some collatz operation
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    else:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+
+print("Введите число для построения последовательнсоти Коллатца:")
+try:
+    i = int(input())
+except ValueError:
+    print("ОШИБКА!!! Введите число")
+else:
+    while i > 1:
+        i = collatz(i)
+
+
