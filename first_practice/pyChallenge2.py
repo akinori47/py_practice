e401d7470e155a3cc55e99750d61fbbd1516f564
Roman <hafaml@Mail.ru>
2019-03-20 12:26:43 +0500

    создан файл с содержимым для челленджа завершен челлендж.

--- first_practice/pyChallenge2.py
+++ first_practice/pyChallenge2.py
@@ -7,11 +7,22 @@ page = request.urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
 contents = page.read()
 page.close()
 
-mess = str(contents.split("-->", 1))
-mess= mess.split("<!--\n", 1)
 
+f = open('content.txt', 'r')        # отрываем файл с содержанием html станицы челленджа
+content = f.read()      # записываем содержимое файла в переменную
 
+items = {}      # создаем пустой словарь
+for item in content:        # считаем кол-во каждого символа в файле
+    if item in items:
+        items[item] = items[item] + 1
+    else:
+        items[item] = 1
 
+if items[item] == 1:        # выводим символы встречающиеся 1 раз
+    for item in items:
+        print(item)
+
+# answer = equality
 
 
 
