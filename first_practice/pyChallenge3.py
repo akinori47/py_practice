4456bf66c8ac0daee49d12615aaf91bfb1cbfcb9
Roman <hafaml@Mail.ru>
2019-03-21 10:44:18 +0500

    add content upload from url

--- first_practice/pyChallenge3.py
+++ first_practice/pyChallenge3.py
@@ -1,12 +1,13 @@
 # Python Challenge #3
 
 from urllib import request
-import string
+
 
 url = 'http://www.pythonchallenge.com/pc/def/equality.html'
 page = request.urlopen(url)
 contents = page.read()
 page.close()
-mess = contents.split
-print(contents)
-print(type(contents))
+mess = contents.split(b'<!--\n')[-1]
+
+print(mess)
+print(type(mess))
